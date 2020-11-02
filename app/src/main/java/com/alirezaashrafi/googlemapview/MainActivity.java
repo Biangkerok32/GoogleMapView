f42115c44af86a94460b538b921392cc8d501222
Biangkerok32 <48493593+Biangkerok32@users.noreply.github.com>
2020-11-02 17:46:56 +0700

    Update MainActivity.java

--- app/src/main/java/com/alirezaashrafi/googlemapview/MainActivity.java
+++ app/src/main/java/com/alirezaashrafi/googlemapview/MainActivity.java
@@ -1,6 +1,6 @@
 package com.alirezaashrafi.googlemapview;
 
-import android.support.v7.app.AppCompatActivity;
+import 	androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.alirezaashrafi.library.GoogleMapView;
